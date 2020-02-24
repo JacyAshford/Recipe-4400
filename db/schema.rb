6e45b4575d4a03d7f691779d4d8cd1d899b86886
JacyAshford <jacyashford@gmail.com>
2020-02-24 16:07:51 -0600

    added image stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_214731) do
+ActiveRecord::Schema.define(version: 2020_02_24_215457) do
 
   create_table "ingredients", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_214731) do
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "steps", force: :cascade do |t|
